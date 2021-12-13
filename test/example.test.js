6e2d9ea5b65446c64de385b1b18fdc2f42130185
Denzel <dbartolaba123@gmail.com>
2021-12-13 15:40:32 -0800

    Adopting a doge 2

--- test/example.test.js
+++ test/example.test.js
@@ -7,26 +7,39 @@ const test = QUnit.test;
 test('Should render a dog Card', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = '<a href=\"./detail/?id=undefined\"><div class=\"dog-card\"><p></p><img src=\"./assets/undefined.jpeg\"></div></a>';
+
+    const testingDog = {
+        name: 'Boogie',
+        breed: 'corgi',
+        id: 0
+    };
+    const expected = '<a href="./detail/?id=0"><div class="dog-card"><p>Boogie</p><img src="./assets/corgi.jpeg"></div></a>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = renderDogCard('Boogie');
+    const actual = renderDogCard(testingDog);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual.outerHTML, expected,);
+    expect.equal(actual.outerHTML, expected, 'Will render card of name, breed, and ID');
 });
 test('Should render the dog detail', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = '<div class=\"dog-detail\"><p class=\"name\"></p><img src=\"../assets/undefined.jpeg\"><div class=\"age-and-breed\"><p class=\"age\">undefined years old</p><p class=\"breed\"></p></div><p class=\"description\"></p></div>';
+    const testDog = {
+        name: 'Boogie',
+        age: '5',
+        breed: 'corgi',
+        description: 'Borkf I am bekom fat snoot shoober tungg, mlem what a nice floof noodle horse. Heckin good boys you are doin me a concern many pats long woofer wrinkler dat tungg tho blop, big ol adorable doggo tungg fluffer. Puggo thicc length boy such treat, thicc. Length boy h*ck yapper tungg blop, woofer borkdrive smol. Maximum borkdrive such treat porgo you are doing me the shock maximum borkdrive, I am bekom fat thicc.'
+
+    };
+    const expected = '<div class="dog-detail"><p class="name">Boogie</p><img src="../assets/corgi.jpeg"><div class="age-and-breed"><p class="age">5 years old</p><p class="breed">corgi</p></div><p class="description">Borkf I am bekom fat snoot shoober tungg, mlem what a nice floof noodle horse. Heckin good boys you are doin me a concern many pats long woofer wrinkler dat tungg tho blop, big ol adorable doggo tungg fluffer. Puggo thicc length boy such treat, thicc. Length boy h*ck yapper tungg blop, woofer borkdrive smol. Maximum borkdrive such treat porgo you are doing me the shock maximum borkdrive, I am bekom fat thicc.</p></div>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = renderDogDetail('Boogie');
+    const actual = renderDogDetail(testDog);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual.outerHTML, expected,);
+    expect.equal(actual.outerHTML, expected, 'Will render the details of name, age, breed and description');
 });
